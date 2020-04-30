f52ffd483bad49a0e8dc6d84d726c86ef57e5803
davin <davin@gmail.com>
2020-04-30 18:31:54 +0900

    fix: sample schema

--- models/Sample.js
+++ models/Sample.js
@@ -3,8 +3,16 @@ const Schema = mongoose.Schema;
 
 const SampleSchema = new Schema({
   name: { type: String, required: true, unique: true },
-  icon: { type: String, required: true, unique: true },
-  position: { type: Array, required: true }
+  cubes: [
+    {
+      position: {
+        x: { type: Number, required: true },
+        y: { type: Number, required: true },
+        z: { type: Number, required: true }
+      },
+      color: { type: String, required: true }
+    }
+  ]
 });
 
 module.exports = mongoose.model('Sample', SampleSchema);
